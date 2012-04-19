2b17930cff9aa91d04c0bfeeb0c3f69e7fd70376
Lorenz H-S <lorenz-dev@lgh-alumni.de>
2012-04-19 12:15:51 +0200

    Add possibility to pass multiple input files; add support for one-time events

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 Stundenplangen
 ==============
 
-This is a simple script that takes as its input a dump of a lecture detail page from KIT's university calendar (Vorlesungsverzeichnis). You can obtain a suitable input file by expanding the list of dates in the lecture detail view, then selecting everything (Ctrl+A), copying it and saving it to a file.
+This is a simple script that takes as its input one or more dumps of a lecture detail page from KIT's university calendar (Vorlesungsverzeichnis). You can obtain a suitable input file by expanding the list of dates in the lecture detail view, then selecting everything (Ctrl+A), copying it and saving it to a file.
 
 The script's output is in iCal format.
 
 Usage
 -----
 
-	python stundenplangen.py inputfilename outputfilename
+	python stundenplangen.py infile1 [infile2 ...] outfile
 
 Requirements
 ------------
@@ -23,4 +23,4 @@ You'll find the exact license in the source code, but it's really just a filled-
 TODO and Warning
 ----------------
 
-This is not exactly neat code and I only tested it on two lectures. Please let me know if it breaks. I also want to add support for multiple input files so you get one big iCal file for your whole timetable.
+This is not exactly neat code and I only tested it on two lectures. Please let me know if it breaks.
--- stundenplan.py
+++ stundenplan.py
@@ -11,16 +11,34 @@ from datetime import datetime, timedelta
 from icalendar import Calendar, Event
 
 class Stundenplanparser:
-    def __init__(self, infilename, outfilename):
-        self.infilename  = infilename
+    def __init__(self, infilenames, outfilename):
+        self.infilenames = infilenames
         self.outfilename = outfilename
         self.dates = []
     
-    def parse(self):
-        with codecs.open(self.infilename, 'r', 'utf-8') as infile:
+    def run(self):
+        for infile in self.infilenames:
+            self.parse(infile)
+        cal = Calendar()
+        cal.add('prodid', '-//Lorenz H-S\'s awesome Stundenplanscript//github.com/lorenzhs/stundenplangen//')
+        cal.add('version', '2.0')
+        cal.add('calscale', 'gregorian')
+        cal.add('X-WR-CALNAME', 'Stundenplan')
+        cal.add('X-WR-TIMEZONE', 'Europe/Berlin')
+        
+        for group in self.dates:
+            title, startdelta, enddelta, location, dates = group
+            for date in dates:
+                cal.add_component(self.addEvent(title, location, date, startdelta, enddelta))
+        
+        with open(self.outfilename, 'w') as outfile:
+            outfile.write(cal.to_ical())
+    
+    def parse(self, infilename):
+        with codecs.open(infilename, 'r', 'utf-8') as infile:
             for line in infile:
                 if u'Titel:\t' in line:
-                    self.title = line[7:-1]
+                    title = line[7:-1]
                     break
             else:
                 print u'Could not find title, please make sure the input file is correct'
@@ -34,43 +52,42 @@ class Stundenplanparser:
             
             acc = None
             while True:
-                acc = self.parseGroup(infile, acc)
+                acc = self.parseGroup(infile, title, acc)
                 if acc is None:
                     break
-            
-            cal = Calendar()
-            cal.add('prodid', '-//Lorenz H-S\'s awesome Stundenplanscript//github.com/lorenzhs/stundenplangen//')
-            cal.add('version', '2.0')
-            cal.add('calscale', 'gregorian')
-            cal.add('X-WR-CALNAME', 'Stundenplan %s' % self.title)
-            cal.add('X-WR-TIMEZONE', 'Europe/Berlin')
-            
-            for group in self.dates:
-                startdelta, enddelta, location, dates = group
-                for date in dates:
-                    cal.add_component(self.addEvent(location, date, startdelta, enddelta))
-            
-            with open(self.outfilename, 'w') as outfile:
-                outfile.write(cal.to_ical())
     
-    def addEvent(self, location,  date, start, end):
+    def addEvent(self, title, location,  date, start, end):
         event = Event()
-        event.add('summary', self.title)
+        event.add('summary', title)
         event.add('dtstart', date + start)
         event.add('dtend', date + end)
         event.add('location', location)
         return event
     
-    def parseGroup(self, infile, rawinfo = None):
+    def parseSingleEvent(self, title, rawinfo):
+        segs = rawinfo.split()
+        date      = self.parseDate(segs[1][1:-2])
+        starttime = [int(x) for x in segs[2].split(':')]
+        endtime   = [int(x) for x in segs[4][:-1].split(':')]
+        location  = ' '.join(segs[5:])
+        startdelta = timedelta(hours = starttime[0], minutes = starttime[1])
+        enddelta   = timedelta(hours = endtime[0],   minutes = endtime[1])
+        self.dates.append((title, startdelta, enddelta, location, [date]))
+        print "Lecture %s on %s starting at %s ending at %s in %s" % (title, date, startdelta, enddelta, location)
+
+    def parseGroup(self, infile, title, rawinfo = None):
         retval = None
         if rawinfo is None:
             rawinfo = infile.readline()
-        if rawinfo == u'Name\tEinrichtungen\n':
+        if rawinfo in [u'Name\tEinrichtungen\n', u' Dozenten\n']:
             return None
+        if rawinfo[-5:-1] == '\t\t\t ':
+            self.parseSingleEvent(title, rawinfo)
+            return infile.readline()
         (startdelta, enddelta, location) = self.parseInfo(rawinfo)
         dates = []
         for line in infile:
-            if line == u' Dozenten\n':
+            if line in [u' Dozenten\n', u'\tAus dieser Serie wurden Termine entfernt.\n']:
                 break
             elif line[0] in string.letters:
                 # Oops, we read the next series' header
@@ -80,8 +97,8 @@ class Stundenplanparser:
                 dates.append(line)
         
         dates = (map(self.parseDate, dates))
-        self.dates.append((startdelta, enddelta, location, dates))
-        print "Lecture %s starting at %s ending at %s in %s" % (self.title, startdelta, enddelta, location)
+        self.dates.append((title, startdelta, enddelta, location, dates))
+        print "Lecture %s starting at %s ending at %s in %s" % (title, startdelta, enddelta, location)
         for termin in dates:
             print "Lecture on %s" % termin
         return retval
@@ -101,8 +118,8 @@ class Stundenplanparser:
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print u'Usage: python %s infile outfile' % sys.argv[0]
-    infilename = sys.argv[1]
-    outfilename = sys.argv[2]
-    parser = Stundenplanparser(infilename, outfilename)
-    parser.parse()
+        print u'Usage: python %s infiles outfile' % sys.argv[0]
+    infilenames = sys.argv[1:-1]
+    outfilename = sys.argv[-1]
+    parser = Stundenplanparser(infilenames, outfilename)
+    parser.run()
